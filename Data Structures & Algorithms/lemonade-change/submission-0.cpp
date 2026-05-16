922d4ed6b7c02d84a8bca4f5a5bfc1ee2304f4e9
Adada Sai Venkat <adadasaivenkat0109@gmail.com>
2026-05-16 21:50:26 +0530

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0,ten=0,twenty=0;
+        for(auto it:bills){
+            if(it==5) five++;
+            else if(it==10){
+                ten++;
+                if(five>0) five--;
+                else return false;
+            }
+            else if(it==20){
+                twenty++;
+                if(ten>0 && five>0){
+                    ten--;
+                    five--;
+                }
+                else if(ten<=0 && five>=3) five-=3;
+                else return false;
+            }
+        }
+        return true;
+    }
+};
