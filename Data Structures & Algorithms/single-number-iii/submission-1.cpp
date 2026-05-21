bf80733bbd5afe34dd8b7c04ce2f35285d7c09df
Adada Sai Venkat <adadasaivenkat0109@gmail.com>
2026-05-21 23:50:25 +0530

    Add: single-number-iii - submission-1

---
+++ Data Structures & Algorithms/single-number-iii/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution{	
+	public:		
+		vector<int> singleNumber(vector<int>& nums){
+            unordered_map<int,int> mpp;
+            for(auto it:nums) mpp[it]++;
+            vector<int> res;
+            for(auto it:nums){
+                if(mpp[it]==1) res.push_back(it);
+            }
+            return res;
+		}
+};
