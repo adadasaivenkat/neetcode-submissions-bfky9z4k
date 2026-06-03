7eb1bd56b37394615cd87de3d8482bae087ecd9a
Adada Sai Venkat <adadasaivenkat0109@gmail.com>
2026-06-03 22:04:37 +0530

    Add: decode-string - submission-0

---
+++ Data Structures & Algorithms/decode-string/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<char> stk;
+        for(auto it:s){
+            if(it==']'){
+                string temp;
+                while(!stk.empty() && stk.top()!='['){
+                    temp+=stk.top();
+                    stk.pop();
+                }
+                stk.pop();
+                string digits;
+                while(!stk.empty() && isdigit(stk.top())){
+                    digits+=stk.top();
+                    stk.pop();
+                }
+                reverse(digits.begin(),digits.end());
+                int num=stoi(digits);
+                // Expanded
+                string exp;
+                for(int i=0; i<num; i++){
+                    exp+=temp;
+                }
+                for(int i=exp.size()-1; i>=0; i--) stk.push(exp[i]);
+            }
+            else stk.push(it);
+        }
+        string res;
+        while(!stk.empty()){
+            res+=stk.top();
+            stk.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
