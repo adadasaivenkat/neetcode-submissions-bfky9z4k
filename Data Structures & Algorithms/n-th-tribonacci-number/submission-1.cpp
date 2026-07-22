6e25431238d12eb9616b73727cbc2d787d6ee61d
Adada Sai Venkat <adadasaivenkat0109@gmail.com>
2026-07-22 21:46:31 +0530

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0) return 0;
+        else if(n==1) return 1;
+        else if(n==2) return 1;
+        else{
+            int a=0,b=1,c=1,sum;
+            for(int i=3; i<=n; i++){
+                sum=a+b+c;
+                a=b;
+                b=c;
+                c=sum;
+            }
+            return sum;
+        }
+    }
+};
