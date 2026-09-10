08062a9b66d8f8715ac432c7e4a13ce53bf1be5b
Adada Sai Venkat <adadasaivenkat0109@gmail.com>
2026-09-10 23:48:11 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.cpp
@@ -0,0 +1,16 @@
+#include <cstring>
+class Solution {
+private:
+    int dp[50];
+    int solve(int n){
+        if(n<0) return 0;
+        if(n==0) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=solve(n-1)+solve(n-2);
+    }
+public:
+    int climbStairs(int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+};
